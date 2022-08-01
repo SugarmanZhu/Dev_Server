3890cd9c041aaf581be9efc31e3dd99d04459629
Xiaotan Zhu <zxt@zhuxiaotan.xyz>
2022-08-01 01:49:19 +0100

    basic bbc rss

---
+++ rss/_next/static/857pW7r_-czQpTUNFXMQ6/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/3-45253a02f3662d82.js","static/chunks/pages/index-0b2385bd4a303234.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()

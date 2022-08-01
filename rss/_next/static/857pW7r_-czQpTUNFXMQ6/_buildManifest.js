1a689392740b0f620f26403e973cfedb44c48ef9
Xiaotan Zhu <zxt@zhuxiaotan.xyz>
2022-08-01 05:56:27 +0100

    add multi source, improve styling

--- rss/_next/static/857pW7r_-czQpTUNFXMQ6/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/3-45253a02f3662d82.js","static/chunks/pages/index-0b2385bd4a303234.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
